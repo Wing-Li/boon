76d89a5321ac5606ecd8801084807c1b5f690d87
Wing_Li <iamlyl@foxmail.com>
2016-06-21 20:55:52 +0800

    增加了颜色

--- app/src/main/java/com/lyl/boon/utils/MyUtils.java
+++ app/src/main/java/com/lyl/boon/utils/MyUtils.java
@@ -1,8 +1,18 @@
 package com.lyl.boon.utils;
 
+import com.lyl.boon.R;
+
+import java.util.Random;
+
 /**
  * Wing_Li
  * 2016/6/21.
  */
 public class MyUtils {
+    public static int[] mTextColors = {R.color.color1, R.color.color2, R.color.color3, R.color.color4, R.color.color5, R.color
+            .color6, R.color.color7, R.color.color8, R.color.color9, R.color.color10};
+
+    public static int getColors() {
+        return mTextColors[new Random().nextInt(11)];
+    }
 }
