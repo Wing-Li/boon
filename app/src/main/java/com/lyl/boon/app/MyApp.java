2e455412c240d6d2a79a0719caef62a1d7711f73
lyl <iamlyl@foxmail.com>
2016-12-21 17:25:10 +0800

    Bugly 的异常分析

--- app/src/main/java/com/lyl/boon/app/MyApp.java
+++ app/src/main/java/com/lyl/boon/app/MyApp.java
@@ -2,6 +2,9 @@ package com.lyl.boon.app;
 
 import android.app.Application;
 
+import com.tencent.bugly.Bugly;
+import com.tencent.bugly.crashreport.CrashReport;
+
 /**
  * Wing_Li
  * 2016/3/31.
@@ -11,6 +14,16 @@ public class MyApp extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        initBugly();
+    }
+
+    /**
+     * 初始化BUG分析 和 应用升级检测
+     */
+    private void initBugly() {
+        CrashReport.UserStrategy strategy = new CrashReport.UserStrategy(getApplicationContext());
+        strategy.setAppReportDelay(60000);
+        Bugly.init(getApplicationContext(), "410b074705", true, strategy);
     }
     
 }
